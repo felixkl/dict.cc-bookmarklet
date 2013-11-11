da0bbbffd38b67a34c0ff7bff20bce78ddcd1ebb
Felix Klemm <johann.klemm@brands4friends.de>
2013-11-11 15:28:57 +0100

    add bookmarklet for dict.cc

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:(function(){var word=function(){var t = '';if(window.getSelection){t=window.getSelection();}else if(document.getSelection){t = document.getSelection();}else if(document.selection){t=document.selection.createRange().text;}return t;};window.open("http://www.dict.cc/?s="+word(),"dict.cc");})();
